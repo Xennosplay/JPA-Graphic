910ae17bb247a7618ea8c822d4e4a592f5387579
Brenno Murakami <brenno.murakami@gmail.com>
2022-09-30 15:36:12 -0300

    Implementacao de interface grafica

---
+++ correcao-avaliacao-atualizado-23-09-2022/correcao-avaliacao/src/br/edu/ifsp/pep/dao/VeiculoDAO.java
@@ -0,0 +1,50 @@
+package br.edu.ifsp.pep.dao;
+
+import br.edu.ifsp.pep.modelo.Veiculo;
+import java.util.List;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author aluno
+ */
+public class VeiculoDAO extends AbstractDAO<Veiculo> {
+
+    public List<Veiculo> buscarTodos() {
+        return getEntityManager()
+                .createNamedQuery("Veiculo.buscarTodos", Veiculo.class)
+                .getResultList();
+    }
+    
+    public List<Veiculo> buscarVeiculosDisponiveisParaLocacao() {
+        return getEntityManager()
+                .createNamedQuery("Veiculo.buscarDisponiveisParaLocacao", Veiculo.class)
+                .getResultList();
+    }
+    
+
+    @Override
+    public void inserir(Veiculo veiculo){
+
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+
+        List<Veiculo> veiculos = em.createNamedQuery(
+                "Veiculo.buscarPorPlacaECidade",
+                Veiculo.class)
+                .setParameter("placa", veiculo.getPlaca())
+                .setParameter("cidade", veiculo.getCidade())
+                .getResultList();
+        if (veiculos == null || veiculos.isEmpty()) {
+            em.persist(veiculo);
+        } else {
+            System.out.println("Já existe um Veículo com a placa "
+                    + veiculo.getPlaca()
+                    + " na cidade " + veiculo.getCidade());
+        }
+
+        em.getTransaction().commit();
+        em.close();
+    }
+
+}
